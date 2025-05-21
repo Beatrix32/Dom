bb656a7f03efa6edcf10bfd05677ad8b1fbe785e
Oz <beatriceb7890@gmail.com>
2025-05-21 21:22:29 +0300

    dom

--- js/spalvos.js
+++ js/spalvos.js
@@ -1,3 +1,7 @@
+import { header } from "./header.js";
+
+header();
+
 document.querySelector('title').textContent = 'Spalvos';
 
 const colors = ['yellow', 'red', 'green', 'blue', 'orange', 'aqua'];
@@ -8,4 +12,3 @@ ulDOM.innerHTML = '';
 for (const color of colors) {
     ulDOM.innerHTML += `<li style="background-color: ${color};">Spalva: ${color}</li>`;
 }
-
